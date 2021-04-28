3ea74f4e521e147764e88d56325dce5edee1d9d5
Tristiisch <tristiisch74@hotmail.com>
2021-04-28 21:03:11 +0200

    up

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 screen="discord"
 file="target/OlympaManager.jar"
 ram_max="1536M"
-screen -XS $screen quit && screen -dmS $screen java -Xmx$ram_max -jar $file
+screen -XS $screen quit
+screen -dmS $screen java -Xmx$ram_max -jar $file
